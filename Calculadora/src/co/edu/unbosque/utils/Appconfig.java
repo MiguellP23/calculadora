622173907acc6b4d4285266cc468db7ce41f9aee
Yishaq Riveros <91137117+Yishaqriv@users.noreply.github.com>
2025-05-06 10:12:40 -0500

    Update Appconfig.java

--- Calculadora/src/co/edu/unbosque/utils/Appconfig.java
+++ Calculadora/src/co/edu/unbosque/utils/Appconfig.java
@@ -3,6 +3,6 @@ package co.edu.unbosque.utils;
 public class Appconfig {
 	
 	public static final String APP_NAME = "Calculadora";
-	public static final String VERSION = "1.0.0";
+	public static final String VERSION = "1.3.0";
 
 }
